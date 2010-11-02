2eddc54951bf084eb46cdc2a45912b58230851f5
Ilya Sabanin <ilya.sabanin@gmail.com>
2010-11-02 19:09:16 +0700

    Fixing HTML handling with Mail gem.

--- lib/postmark/message_extensions/mail.rb
+++ lib/postmark/message_extensions/mail.rb
@@ -3,8 +3,14 @@ module Mail
     
     include Postmark::SharedMessageExtensions
     
+    def html?
+      content_type.include?('text/html')
+    end
+    
     def body_html
-      unless html_part.nil?
+      if html_part.nil?
+        body.to_s if html?
+      else
         html_part.body.to_s
       end
     end
