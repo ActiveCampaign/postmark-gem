492573bb8197380adc532ebac546b27e33449087
Anton Astashov <anton.astashov@gmail.com>
2012-03-01 16:07:00 +0800

    Small code improvements

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rubygems'
+require 'bundler'
+Bundler.setup(:development)
 require 'mail'
 #require 'tmail'
 require 'postmark'
