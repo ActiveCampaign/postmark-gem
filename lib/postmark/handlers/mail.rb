3b579d35ba67e26a1ae26bcbe4e51ae249f81ad9
Artem Chistyakov <tema@wildbit.com>
2012-11-02 16:45:54 +0800

    Save a received MessageID in message headers.

--- lib/postmark/handlers/mail.rb
+++ lib/postmark/handlers/mail.rb
@@ -1,16 +1,17 @@
 module Mail
   class Postmark
-    
+
     attr_accessor :settings
-    
+
     def initialize(values)
       self.settings = { :api_key => nil }.merge(values)
     end
-    
+
     def deliver!(mail)
       ::Postmark.api_key = settings[:api_key]
-      ::Postmark.send_through_postmark(mail)
+      response = ::Postmark.send_through_postmark(mail)
+      mail["Message-ID"] = response["MessageID"] if response.kind_of?(Hash)
     end
-    
+
   end
 end
