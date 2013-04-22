1caf286015c47141d0c9d8928dedabb960e426cc
Artem Chistyakov <tema@wildbit.com>
2013-04-22 15:01:20 +0800

    Add a few unit tests for attachments API.

--- spec/unit/message_extensions/mail_spec.rb
+++ spec/unit/message_extensions/mail_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe Mail::Message do
+  before do
+    Kernel.stub(:warn)
+  end
+
   let(:mail_message) do
     Mail.new do
       from    "sheldon@bigbangtheory.com"
@@ -50,6 +54,66 @@ describe Mail::Message do
     it 'returns text body if present' do
       mail_message.body_text.should == "Hello Sheldon!"
     end
+  end
+
+  describe "#postmark_attachments=" do
+    let(:attached_hash) { {'Name' => 'picture.jpeg',
+                           'ContentType' => 'image/jpeg'} }
+
+    it "stores attachments as an array" do
+      mail_message.postmark_attachments = attached_hash
+      mail_message.instance_variable_get(:@_attachments).should include(attached_hash)
+    end
+
+    it "is deprecated" do
+      Kernel.should_receive(:warn).with(/deprecated/)
+      mail_message.postmark_attachments = attached_hash
+    end
+  end
+
+  describe "#postmark_attachments" do
+    let(:attached_file) { mock("file") }
+    let(:attached_hash) { {'Name' => 'picture.jpeg',
+                           'ContentType' => 'image/jpeg'} }
+    let(:exported_file) { {'Name' => 'file.jpeg',
+                           'ContentType' => 'application/octet-stream',
+                           'Content' => ''} }
+
+    before do
+      attached_file.stub(:is_a?) { |arg| arg == File ? true : false }
+      attached_file.stub(:path) { '/tmp/file.jpeg' }
+    end
+
+    it "supports multiple attachment formats" do
+      IO.should_receive(:read).with("/tmp/file.jpeg").and_return("")
+
+      mail_message.postmark_attachments = [attached_hash, attached_file]
+      attachments = mail_message.export_attachments
+
+      attachments.should include(attached_hash)
+      attachments.should include(exported_file)
+    end
+
+    it "is deprecated" do
+      Kernel.should_receive(:warn).with(/deprecated/)
+      mail_message.postmark_attachments
+    end
+  end
+
+  describe "#export_attachments" do
+    let(:file_data) { 'binarydatahere' }
+    let(:exported_data) do
+      {'Name' => 'face.jpeg',
+       'Content' => "YmluYXJ5ZGF0YWhlcmU=\n",
+       'ContentType' => 'image/jpeg'}
+    end
+
+    it "exports native attachments" do
+      mail_message.attachments["face.jpeg"] = file_data
+      mail_message.export_attachments.should include(exported_data)
+    end
+
+    it "still the deprecated attachments API"
   end
 
   describe "#to_postmark_hash" do
