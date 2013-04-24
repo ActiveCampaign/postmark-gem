37aed7cf34f2c83c63630dd17210dd3100b3ec27
temochka <chistyakov.artem@gmail.com>
2013-04-24 22:04:03 +0800

    Add integration tests for batch delivery.

--- spec/integration/delivery_spec.rb
+++ spec/integration/delivery_spec.rb
@@ -3,29 +3,30 @@ require 'spec_helper'
 describe "Sending emails with Postmark" do
   let(:postmark_message_id_format) { /\w{8}\-\w{4}-\w{4}-\w{4}-\w{12}/ }
 
+  let(:message) {
+    Mail.new do
+      from "sender@postmarkapp.com"
+      to "recipient@postmarkapp.com"
+      subject "Mail::Message object"
+      body "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do "
+           "eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      delivery_method Mail::Postmark, :api_key => "POSTMARK_API_TEST"
+    end
+  }
+
+  let(:message_with_no_body) {
+    Mail.new do
+      from "sender@postmarkapp.com"
+      to "recipient@postmarkapp.com"
+      delivery_method Mail::Postmark, :api_key => "POSTMARK_API_TEST"
+    end
+  }
+
   context "Mail::Postmark delivery method" do
-    let(:message) {
-      Mail.new do
-        from "sender@postmarkapp.com"
-        to "recipient@postmarkapp.com"
-        subject "Mail::Message object"
-        body "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do "
-             "eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        delivery_method Mail::Postmark, :api_key => "POSTMARK_API_TEST"
-      end
-    }
 
     let(:message_with_attachment) {
       message.tap do |msg|
         msg.attachments["test.gif"] = File.read(File.join(File.dirname(__FILE__), '..', 'data', 'empty.gif'))
-      end
-    }
-
-    let(:message_with_no_body) {
-      Mail.new do
-        from "sender@postmarkapp.com"
-        to "recipient@postmarkapp.com"
-        delivery_method Mail::Postmark, :api_key => "POSTMARK_API_TEST"
       end
     }
 
@@ -70,9 +71,44 @@ describe "Sending emails with Postmark" do
   end
 
   context "batch delivery" do
-    it 'should deliver a batch of Mail::Message objects' do
-      pending
-      # subject.deliver_messages([message, message_with_attachment, message]).should be
+    let(:api_client) { Postmark::ApiClient.new('POSTMARK_API_TEST') }
+    let(:valid_messages) { [message, message.dup] }
+    let(:partially_valid_messages) { [message, message.dup, message_with_no_body] }
+    let(:invalid_messages) { [message_with_no_body, message_with_no_body.dup] }
+
+    it 'delivers a batch of valid Mail::Message objects' do
+      expect { api_client.deliver_messages(valid_messages) }.
+          to change{valid_messages.all? { |m| m.delivered? }}.
+             to true
     end
+
+    it 'updates delivered messages with Message-IDs' do
+      api_client.deliver_messages(valid_messages)
+
+      expect(valid_messages.all? { |m| m.message_id =~ postmark_message_id_format }).
+          to be_true
+    end
+
+    it 'updates delivered messages with related Postmark responses' do
+      api_client.deliver_messages(valid_messages)
+
+      expect(valid_messages.all? { |m| m.postmark_response["To"] == m.to[0] }).
+          to be_true
+    end
+
+    it 'partially delivers a batch of partially valid Mail::Message objects' do
+      expect { api_client.deliver_messages(partially_valid_messages) }.
+          to change{partially_valid_messages.select { |m| m.delivered? }.count}.
+             to 2
+    end
+
+    it "doesn't deliver a batch of invalid Mail::Message objects" do
+      expect { api_client.deliver_messages(invalid_messages) }.
+          to change{invalid_messages.all? { |m| m.delivered? == false }}.
+             to true
+
+      invalid_messages.should satisfy { |ms| ms.all? { |m| !!m.postmark_response }}
+    end
+
   end
 end
