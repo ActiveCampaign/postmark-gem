b698001fdcfbaf58e62ffc0510b8f25b074bd387
temochka <chistyakov.artem@gmail.com>
2013-04-29 20:30:23 +0800

    Make Postmark::Inflector idempotent.

--- lib/postmark/inflector.rb
+++ lib/postmark/inflector.rb
@@ -4,11 +4,21 @@ module Postmark
     extend self
 
     def to_postmark(name)
-      name.to_s.split('_').map { |part| part.capitalize }.join('')
+      name.to_s.split('_').map { |part| capitalize_first_letter(part) }.join('')
     end
 
     def to_ruby(name)
       name.scan(/(?:[A-Z](?:(?:[A-Z]+(?![a-z]))|[a-z]*))/).join('_').downcase.to_sym
+    end
+
+    protected
+
+    def capitalize_first_letter(str)
+      if str.length > 0
+        str.slice(0).capitalize + str.slice(1..-1)
+      else
+        str
+      end
     end
   end
 end
