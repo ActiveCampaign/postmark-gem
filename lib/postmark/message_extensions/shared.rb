cde484f90a699e302ac77b6baa70f4b8f19786de
temochka <chistyakov.artem@gmail.com>
2013-04-29 20:33:05 +0800

    Use Postmark::MessageHelper in Postmark::SharedMessageExtensions to avoid duplication of code.

--- lib/postmark/message_extensions/shared.rb
+++ lib/postmark/message_extensions/shared.rb
@@ -23,7 +23,7 @@ module Postmark
       Kernel.warn("Mail::Message#postmark_attachments= is deprecated and will " \
                   "be removed in the future. Please consider using the native " \
                   "attachments API provided by Mail library.")
-      @_attachments = wrap_in_array(value)
+      @_attachments = value
     end
 
     def postmark_attachments
@@ -32,34 +32,13 @@ module Postmark
                   "be removed in the future. Please consider using the native " \
                   "attachments API provided by Mail library.")
 
-      @_attachments.map do |item|
-        if item.is_a?(Hash)
-          item
-        elsif item.is_a?(File)
-          {
-            "Name"        => item.path.split("/")[-1],
-            "Content"     => pack_attachment_data(IO.read(item.path)),
-            "ContentType" => "application/octet-stream"
-          }
-        end
-      end
+      Postmark::MessageHelper.attachments_to_postmark(@_attachments)
     end
 
     protected
 
     def pack_attachment_data(data)
-      [data].pack('m')
-    end
-
-    # From ActiveSupport (Array#wrap)
-    def wrap_in_array(object)
-      if object.nil?
-        []
-      elsif object.respond_to?(:to_ary)
-        object.to_ary || [object]
-      else
-        [object]
-      end
+      MessageHelper.encode_in_base64(data)
     end
 
   end
