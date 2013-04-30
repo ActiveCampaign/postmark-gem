da57b0c82ffc27302b7464cdb4977c7aeb695eec
temochka <chistyakov.artem@gmail.com>
2013-04-30 13:10:08 +0800

    Make Postmark::HashHelper.to_ruby idempotent.

--- lib/postmark/helpers/hash_helper.rb
+++ lib/postmark/helpers/hash_helper.rb
@@ -4,11 +4,31 @@ module Postmark
     extend self
 
     def to_postmark(hash)
-      hash.inject({}) { |memo, (k,v)| memo[Inflector.to_postmark(k)] = v; memo }
+      hash.inject({}) { |m, (k,v)| m[Inflector.to_postmark(k)] = v; m }
     end
 
-    def to_ruby(hash)
-      hash.inject({}) { |memo, (k,v)| memo[Inflector.to_ruby(k)] = v; memo }
+    def to_ruby(hash, compatible = false)
+      formatted = hash.inject({}) { |m, (k,v)| m[Inflector.to_ruby(k)] = v; m }
+
+      if compatible
+        formatted.merge!(hash)
+        enhance_with_compatibility_warning(formatted)
+      end
+
+      formatted
+    end
+
+    protected
+
+    def enhance_with_compatibility_warning(hash)
+      def hash.[](key)
+        if key.is_a? String
+          Kernel.warn("Postmark: the CamelCased String keys of response are " \
+                      "deprecated in favor of underscored symbols. The " \
+                      "support will be dropped in the future.")
+        end
+        super
+      end
     end
 
   end
--- spec/unit/postmark/helpers/hash_helper_spec.rb
+++ spec/unit/postmark/helpers/hash_helper_spec.rb
@@ -1,8 +1,7 @@
 require 'spec_helper'
 
 describe Postmark::HashHelper do
-
-  describe ".hash_to_postmark" do
+  describe ".to_postmark" do
     let(:source) { {:from => "support@postmarkapp.com", :reply_to => "contact@wildbit.com"} }
     let(:target) { {"From" => "support@postmarkapp.com", "ReplyTo" => "contact@wildbit.com"} }
 
@@ -12,6 +11,23 @@ describe Postmark::HashHelper do
 
     it 'acts idempotentely' do
       subject.to_postmark(target).should == target
+    end
+  end
+
+  describe ".to_ruby" do
+    let(:source) { {"From" => "support@postmarkapp.com", "ReplyTo" => "contact@wildbit.com"} }
+    let(:target) { {:from => "support@postmarkapp.com", :reply_to => "contact@wildbit.com"} }
+
+    it 'converts Hash keys to Ruby format' do
+      subject.to_ruby(source).should == target
+    end
+
+    it 'has compatible mode' do
+      subject.to_ruby(source, true).should == target.merge(source)
+    end
+
+    it 'acts idempotentely' do
+      subject.to_ruby(target).should == target
     end
   end
 
