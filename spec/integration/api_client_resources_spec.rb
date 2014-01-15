8d0d32141f571b9f7906c93f5fed72a7421769a9
Artem Chistyakov <chistyakov.artem@gmail.com>
2014-01-15 17:18:21 -0500

    Update integration tests for Messages API.

--- spec/integration/api_client_resources_spec.rb
+++ spec/integration/api_client_resources_spec.rb
@@ -19,24 +19,44 @@ describe 'Accessing server resources using the API' do
 
     def with_retries(max_retries = 10, wait_seconds = 3)
       yield
-    rescue Postmark::DeliveryError
+    rescue => e
       retries = retries ? retries + 1 : 1
       if retries < max_retries
         sleep wait_seconds
         retry
       else
-        raise
+        raise e
       end
     end
 
-    it 'is possible to send a message and access it via the Messages API' do
+    it 'is possible to send a message and access its details via the Messages API' do
       response = api_client.deliver(message)
       message = with_retries {
         api_client.get_message(response[:message_id])
       }
       expect(message[:recipients]).to include(recipient)
-      dump = api_client.dump_message(response[:message_id])
+    end
+
+    it 'is possible to send a message and dump it via the Messages API' do
+      response = api_client.deliver(message)
+      dump = with_retries {
+        api_client.dump_message(response[:message_id])
+      }
       expect(dump[:body]).to include('Mail::Message object')
+    end
+
+    it 'is possible to send a message and find it via the Messages API' do
+      response = api_client.deliver(message)
+      expect {
+        with_retries {
+          messages = api_client.get_messages(:recipient => recipient,
+                                  :fromemail => message[:from],
+                                  :subject => message[:subject])
+          unless messages.map { |m| m[:message_id] }.include?(response[:message_id])
+            raise 'Message not found'
+          end
+        }
+      }.not_to raise_error
     end
 
   end
