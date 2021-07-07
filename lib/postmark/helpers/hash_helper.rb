e252810fcc6d2d2cf80402f6e5e00e9bdd011431
Igor Balos <ibalosh@gmail.com>
2021-07-07 17:36:15 +0200

    fixed broken tests

--- lib/postmark/helpers/hash_helper.rb
+++ lib/postmark/helpers/hash_helper.rb
@@ -1,18 +1,23 @@
 module Postmark
   module HashHelper
-
     extend self
 
-    def to_postmark(hash)
-      hash.inject({}) { |m, (k,v)| m[Inflector.to_postmark(k)] = object_value_to_postmark(v); m }
+    def to_postmark(hash, keys_to_skip: [])
+      hash.each_with_object({}) do |(k, v), m|
+        m[Inflector.to_postmark(k)] = skip_key?(k, keys_to_skip) ? v : hash_value_to_postmark(v)
+      end
     end
 
-    def to_ruby(hash, compatible = false)
-      formatted = hash.inject({}) { |m, (k,v)| m[Inflector.to_ruby(k)] = object_value_to_ruby(v); m }
-      compatible ? to_ruby_with_compatibility(hash, formatted) : formatted
+    def to_ruby(hash, compatibility_mode = false)
+      formatted = hash.each_with_object({}) { |(k, v), m| m[Inflector.to_ruby(k)] = hash_value_to_ruby(v); }
+      compatibility_mode ? to_ruby_with_compatibility(hash, formatted) : formatted
     end
 
-    protected
+    private
+
+    def skip_key?(key, keys_to_skip)
+      keys_to_skip.map { |k| k.to_s.downcase.strip }.include?(key.to_s.downcase.strip)
+    end
 
     def to_ruby_with_compatibility(hash, formatted)
       formatted.merge!(hash)
@@ -23,26 +28,24 @@ module Postmark
     def enhance_with_compatibility_warning(hash)
       def hash.[](key)
         if key.is_a? String
-          Kernel.warn("Postmark: the CamelCased String keys of response are " \
-                      "deprecated in favor of underscored symbols. The " \
-                      "support will be dropped in the future.")
+          Kernel.warn('Postmark: the CamelCased String keys of response are ' \
+                      'deprecated in favor of underscored symbols. The ' \
+                      'support will be dropped in the future.')
         end
         super
       end
     end
 
-    private
-
-    def object_value_to_postmark(value)
+    def hash_value_to_postmark(value)
       return to_postmark(value) if value.is_a?(Hash)
-      return value.map { |entry| to_postmark(entry) } if value.is_a?(Array)
+      return value.map { |entry| hash_value_to_postmark(entry) } if value.is_a?(Array)
 
       value
     end
 
-    def object_value_to_ruby(value)
+    def hash_value_to_ruby(value)
       return to_ruby(value) if value.is_a?(Hash)
-      return value.map { |entry| to_ruby(entry) } if value.is_a?(Array)
+      return value.map { |entry| hash_value_to_ruby(entry) } if value.is_a?(Array)
 
       value
     end
